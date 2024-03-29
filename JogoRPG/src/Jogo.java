53e032182c8922b290e9dd4a9eb913ecf6cac4a4
Érica Vieira <ericaj.vieira13@gmail.com>
2024-03-28 22:43:12 -0300

    Fazendo menu

--- JogoRPG/src/Jogo.java
+++ JogoRPG/src/Jogo.java
@@ -13,15 +13,45 @@ class Jogo {
 
     public void iniciarJogo() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Bem-vindo ao jogo RPG!");
-        System.out.println("Você se depara com um temível dragão! Prepare-se para a batalha!");
-        System.out.println("-------------------------------");
+
+        System.out.println("====================================");
+        System.out.println("  Bem-vindo ao Jogo de RPG Fantasia " );
+        System.out.println("------------------------------------");
+        System.out.println("Você se depara com um temível dragão!\nPrepare-se para a batalha!");
+        System.out.println("====================================");
+        System.out.println("|                                  |");
+        System.out.println("|        Escolha seu Herói:        |");
+        System.out.println("|                                  |");
+        System.out.println("|  1. Gandolf - O Mago             |");
+        System.out.println("|  2. Aragon - O Guerreiro         |");
+        System.out.println("|  3. Sair do Jogo                 |");
+        System.out.println("|                                  |");
+        System.out.println("====================================");
+        int escolha;
+            do {
+                System.out.print("Escolha seu Herói: ");
+               escolha = scanner.nextInt();
+
+                switch (escolha) {
+                    case 1:
+                        System.out.println("Gandolf");
+                        break;
+                    case 2:
+                        System.out.println("Aragon");
+                        break;
+                    case 3:
+                        System.out.println("Você saiu do jogo. Até logo!");
+                        return;
+                    default:
+                        System.out.println("Opção inválida. Por favor, escolha novamente.");
+                }
+            } while (escolha < 1 || escolha > 3);
 
         while (jogador.estaVivo() && inimigo.estaVivo()) {
             System.out.println("Escolha sua ação:");
             System.out.println("1. Atacar");
             System.out.println("2. Defender");
-            int escolha = scanner.nextInt();
+             escolha = scanner.nextInt();
 
             if (escolha == 1) {
                 jogador.atacar(inimigo);
