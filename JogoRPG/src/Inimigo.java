8551df86f9c2e52275824c6d5fc4d48eebf957e1
rayla silva <raylaferrnanda@gmail.com>
2024-03-29 22:18:28 -0300

    subindo alterações: metodo de defesa

--- JogoRPG/src/Inimigo.java
+++ JogoRPG/src/Inimigo.java
@@ -1,27 +1,38 @@
 import java.util.Random;
 
-class Inimigo {
+public class Inimigo {
     protected int vida;
     protected int ataque;
+
+    protected int defesa;
 
     public Inimigo() {
         this.vida = 200;
         this.ataque = 15;
+        this.defesa = 5;
     }
 
     public void atacar(Jogador jogador) {
+
+        //Jogador perde vida atacando o dragão
         System.out.println("O dragão ataca você!");
         int dano = this.ataque;
-        if (jogador.defendendo) {
+        // Jogador perde vida se defendendo
+        if (jogador.defesa > 0) {
             // COLOQUEI DO DANO AQUI, QUE ESTAVA EM JOGADOR
-            dano /= 2;
-            jogador.defendendo = false;
+            dano = this.defesa;
+
         }
         jogador.vida -= dano;
-        System.out.println("Sua vida: " + jogador.vida);
+
+        //Para não mostrar vidas em negativos
+        if (jogador.vida > 0) {
+            System.out.println("Sua vida: " + jogador.vida);
+        }
     }
 
-    public boolean estaVivo() {
-        return vida > 0;
+
+        public boolean estaVivo () {
+            return vida > 0;
+        }
     }
-}
